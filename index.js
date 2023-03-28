5ba9a4f3c0f68167135adba383d2f3731a7df8e1
Kennedy Muinde <kennedymuinde@gmail.com>
2023-03-28 12:36:10 +0300

    Almost finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+function submitData(name, email) {
+  const config = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      email,
+    }),
+  };
+  return fetch("http://localhost:3000/users", config)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Error");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      document.body.innerHTML = data;
+    })
+    .catch((error) => {
+      document.body.innerHTML = error;
+    });
+}
